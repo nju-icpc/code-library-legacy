ad09d925289f83f0a7eaa451145e20d73843215b
SRC Ace <SRC Ace>
2017-04-13 01:31:31 +0100

    quadratic-residue

--- src/acesrc/quadratic-residue.cpp
+++ src/acesrc/quadratic-residue.cpp
@@ -1,7 +1,7 @@
 /*
-    Find the smallest x (0 <= x < m, such that x^2 = n (mod m)).
-    If not exists, return -1.
-    m is a prime
+  Find the smallest x (0 <= x < m, such that x^2 = n (mod m)).
+  If not exists, return -1.
+  m is a prime
 */
 #include <stdio.h>
 #include <iostream>
@@ -9,69 +9,71 @@
 using namespace std;
 
 struct QuadraticResidue {
-    typedef long long int64;
-    typedef pair<int64, int64> QuadraticNum;
+  typedef long long int64;
+  typedef pair<int64, int64> QuadraticNum;
 
-    inline int64 Sqr(int64 a) {
-        return a * a;
-    }
+  inline int64 Sqr(int64 a) {
+    return a * a;
+  }
 
-    int64 Exp(int64 a, int64 n, int64 mod) {
-        if (n == 0) return 1;
-        if (n & 1) return Sqr(Exp(a, n >> 1, mod)) % mod * a % mod;
-        else return Sqr(Exp(a, n >> 1, mod)) % mod;
-    }
+  int64 Exp(int64 a, int64 n, int64 mod) {
+    if (n == 0) return 1;
+    if (n & 1) return Sqr(Exp(a, n >> 1, mod)) % mod * a % mod;
+    else return Sqr(Exp(a, n >> 1, mod)) % mod;
+  }
 
-    int64 inx;
-    QuadraticNum QuadraticMul(const QuadraticNum a, const QuadraticNum b, const int mod) {
-        QuadraticNum ans;
-        ans.first = (a.first * b.first % mod + a.second * b.second % mod * inx % mod) % mod;
-        ans.second = (a.first * b.second % mod + a.second * b.first % mod) % mod;
-        return ans;
-    }
+  int64 inx;
+  QuadraticNum QuadraticMul(const QuadraticNum a, const QuadraticNum b, const int mod) {
+    QuadraticNum ans;
+    ans.first = (a.first * b.first % mod + a.second * b.second % mod * inx % mod) % mod;
+    ans.second = (a.first * b.second % mod + a.second * b.first % mod) % mod;
+    return ans;
+  }
 
-    QuadraticNum Sqr(QuadraticNum a, int mod) {
-        return QuadraticMul(a, a, mod);
-    }
+  QuadraticNum Sqr(QuadraticNum a, int mod) {
+    return QuadraticMul(a, a, mod);
+  }
 
-    QuadraticNum Exp(QuadraticNum a, int n, int mod) {
-        if (n == 0) return QuadraticNum(1, 0);
-        if (n & 1) return QuadraticMul(Sqr(Exp(a, n >> 1, mod), mod), a, mod);
-        else return Sqr(Exp(a, n >> 1, mod), mod);
-    }
+  QuadraticNum Exp(QuadraticNum a, int n, int mod) {
+    if (n == 0) return QuadraticNum(1, 0);
+    if (n & 1) return QuadraticMul(Sqr(Exp(a, n >> 1, mod), mod), a, mod);
+    else return Sqr(Exp(a, n >> 1, mod), mod);
+  }
 
-    int GetRoot(int n, int m) {
-        n = (n % m + m) % m;
-        if (n == 0) return 0;
-        if (m == 2) return 1;
-        if ((Exp(n, (m - 1) / 2, m) + m) % m == m - 1) return -1;
-        // x does not exist.
-        while (true) {
-            int i = rand() % m;
-            inx = ((int64)i * i - n) % m;
-            if ((Exp(inx, (m - 1) / 2, m) + m) % m == m - 1) {
-                int x = (Exp(QuadraticNum(i, 1), (m + 1) / 2, m).first + m) % m;
-                int y = m - x;
-                if (x > y) swap(x, y);
-                return x;
-            }
-        }
+  int GetRoot(int n, int m) {
+    n = (n % m + m) % m;
+    if (n == 0) return 0;
+    if (m == 2) return 1;
+    if ((Exp(n, (m - 1) / 2, m) + m) % m == m - 1) return -1;
+    // x does not exist.
+    while (true) {
+      int i = rand() % m;
+      inx = ((int64)i * i - n) % m;
+      if ((Exp(inx, (m - 1) / 2, m) + m) % m == m - 1) {
+        int x = (Exp(QuadraticNum(i, 1), (m + 1) / 2, m).first + m) % m;
+        int y = m - x;
+        if (x > y) swap(x, y);
+        return x;
+      }
     }
+  }
 };
 
+// This shows how to use it. 
+// You can submit on http://acm.timus.ru/problem.aspx?space=1&num=1132
 int main() {
-    int t, n, m;
-    QuadraticResidue quadratic_residue;
-    scanf("%d", &t);
-    while (t--) {
-        scanf("%d%d", &n, &m);
-        int root = quadratic_residue.GetRoot(n, m);
-        if (root == -1)
-            printf("No root\n");
-        else {
-            if (root + root == m) printf("%d\n", root);
-            else printf("%d %d\n", root, m - root);
-        }
+  int t, n, m;
+  QuadraticResidue quadratic_residue;
+  scanf("%d", &t);
+  while (t--) {
+    scanf("%d%d", &n, &m);
+    int root = quadratic_residue.GetRoot(n, m);
+    if (root == -1)
+      printf("No root\n");
+    else {
+      if (root + root == m) printf("%d\n", root);
+      else printf("%d %d\n", root, m - root);
     }
-    return 0;
+  }
+  return 0;
 }
