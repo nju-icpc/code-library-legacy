ec75b86db12fd6659584a7b2bb7981b885b39fa3
massimodong <massimodong@users.noreply.github.com>
2017-10-24 22:24:38 +0800

    Update io.cpp

--- src/config/io.cpp
+++ src/config/io.cpp
@@ -2,6 +2,7 @@
 char buf[BUFSIZE], *pt = buf;
 #define scan(tt) do { \
   int &t = tt; \
+  t=0;
   while (!((*pt) >= '0' && (*pt) <= '9')) pt ++; \
   while (((*pt) >= '0' && (*pt) <= '9')) t = t * 10 + (*(pt ++)) - '0'; \
 } while (0)
